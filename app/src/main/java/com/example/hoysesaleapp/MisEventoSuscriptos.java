b7dcd920246374ff00797e6b6986fb37a74e89a9
Cecilia Quintana <ceciquin@protonmail.ch>
2019-11-04 10:55:24 -0300

    perfil, buscar evento, resultado busqueda evento.

--- app/src/main/java/com/example/hoysesaleapp/MisEventoSuscriptos.java
+++ app/src/main/java/com/example/hoysesaleapp/MisEventoSuscriptos.java
@@ -10,5 +10,18 @@ public class MisEventoSuscriptos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_evento_suscriptos);
+        /*
+        final Spinner spinner = (Spinner) findViewById(R.id.filtroCategorias);
+        // Initializing a String Array
+        String[] plants = new String[]{
+                "Seleccione una categaria",
+                "zona",
+                "Deporte",
+                "WTecnologia",
+                "Literatura",
+                "Idiomas"}*/
+
+        // para desplegar los eventos suscriptos en la listView usar el adaptador y el layout de buscar evento.
     }
+
 }
--- app/src/main/java/com/example/hoysesaleapp/MisEventosCreados.java
+++ app/src/main/java/com/example/hoysesaleapp/MisEventosCreados.java
@@ -10,5 +10,17 @@ public class MisEventosCreados extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_eventos_creados);
+        /*
+        final Spinner spinner = (Spinner) findViewById(R.id.filtroCategorias);
+        // Initializing a String Array
+        String[] plants = new String[]{
+                "Seleccione una categaria",
+                "zona",
+                "Deporte",
+                "WTecnologia",
+                "Literatura",
+                "Idiomas"}*/
+
+        // para desplegar los eventos creados en la listView usar el adaptador y el layout de buscar evento.*/
     }
 }
