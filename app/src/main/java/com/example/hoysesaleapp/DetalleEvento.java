fb8aafdd8414e757a6931cf7f5e587b2a37e8d00
Cecilia Quintana <ceciquin@protonmail.ch>
2019-11-04 00:39:18 -0300

    Pantalla Evento suscripto, evento creado y detalle evento

---
+++ app/src/main/java/com/example/hoysesaleapp/DetalleEvento.java
@@ -0,0 +1,14 @@
+package com.example.hoysesaleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_evento);
+    }
+}
